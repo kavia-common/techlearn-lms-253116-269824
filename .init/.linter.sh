d832c65eb82a081c40f82cd2e049e10ceef19f57
Kavia AI <production@kavia.ai>
2025-11-19 05:38:17 +0000

    Checkpoint 1 - cm9adfa20d

--- .init/.linter.sh
+++ .init/.linter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/kavia/workspace/code-generation/techlearn-lms-253116/lms_frontend
+cd /home/kavia/workspace/code-generation/techlearn-lms-253116-269824/lms_frontend
 npm run build
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
